53047efd61e924af99faa7a5e47da0db062198f9
kunwaraman <amankunwar8472@gmail.com>
2025-03-03 20:49:31 +0530

    day3

---
+++ PartitionArrayAccordingToGivenPivot.java
@@ -0,0 +1,26 @@
+public class PartitionArrayAccordingToGivenPivot {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        int j = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<pivot){
+                ans[j]=nums[i];
+                j++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]==pivot){
+                ans[j]=nums[i];
+                j++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>pivot){
+                ans[j]=nums[i];
+                j++;
+            }
+        }
+        return ans;
+    }
+}
