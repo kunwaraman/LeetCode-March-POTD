a856193d336aa9531eef041895b77b7415b3631b
kunwaraman <amankunwar8472@gmail.com>
2025-03-19 20:10:02 +0530

    day19

---
+++ MaximumOperationsToMakeBinaryArrayEqualto1.java
@@ -0,0 +1,26 @@
+/*
+import java.util.*;
+
+time complexity: O(n)
+space complexity: O(1)
+ */
+
+public class MaximumOperationsToMakeBinaryArrayEqualto1 {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int count=0;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]==0){
+                nums[i]=1-nums[i];
+                nums[i+1]=1-nums[i+1];
+                nums[i+2]=1-nums[i+2];
+                count++;
+            }
+
+        }
+        if(nums[n-1]==0||nums[n-2]==0){
+            return -1;
+        }
+        return count;
+    }
+}
