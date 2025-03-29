01d589800bbf148f4fb0d0de39c729c1993579b8
kunwaraman <amankunwar8472@gmail.com>
2025-03-29 17:21:50 +0530

    optmd->28

--- MaximumNumberOfPointsFromGrids.java
+++ MaximumNumberOfPointsFromGrids.java
@@ -44,8 +44,57 @@ public class MaximumNumberOfPointsFromGrids {
 
             }
             return count;
+        }
+        /*
+        optimised
+
+         */
+        class Solution {
 
 
+            public int[] maxPoints(int[][] grid, int[] queries) {
+                int m = grid.length;
+                int n = grid[0].length;
+                int q = queries.length;
+                int ans[] = new int[q];
+
+                int[][] sq = new int[q][2];
+                for (int i = 0; i < q; i++) {
+                    sq[i][0] = queries[i];
+                    sq[i][1] = i;
+                }
+                Arrays.sort(sq, Comparator.comparingInt(a -> a[0]));
+
+                PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+                boolean[][] visited = new boolean[m][n];
+                int points = 0;
+
+                pq.offer(new int[]{grid[0][0], 0, 0});
+                visited[0][0] = true;
+
+                for (int i = 0; i < q; i++) {
+                    int qv = sq[i][0];
+                    int idx = sq[i][1];
+
+                    while (!pq.isEmpty() && pq.peek()[0] < qv) {
+                        int[] top = pq.poll();
+                        int x = top[1], y = top[2];
+                        points++;
+
+                        for (int[] dir : direct) {
+                            int i_ = x + dir[0];
+                            int j_ = y + dir[1];
+                            if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n && !visited[i_][j_]) {
+                                pq.offer(new int[]{grid[i_][j_], i_, j_});
+                                visited[i_][j_] = true;
+                            }
+                        }
+                    }
+                    ans[idx] = points;
+                }
+
+                return ans;
+            }
         }
 
 }
